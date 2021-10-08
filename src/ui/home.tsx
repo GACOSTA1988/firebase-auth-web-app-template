d0643070114a5f4c705d01427c21b0c5365990e7
GACOSTA1988 <georgeacostar@gmail.com>
2021-10-07 18:50:18 -0700

    adds navigation callbacks

--- src/ui/home.tsx
+++ src/ui/home.tsx
@@ -6,15 +6,19 @@ import Button from "@mui/material/Button"
 
 const HomePage: React.FC<IPage> = (props) => {
   const history = useHistory()
-  console.log("history:", history)
 
   useEffect(() => {
     logging.info(`Loading ${props.name}`)
   }, [])
+
+  const handleOnClick = () => {
+    history.push("/")
+  }
+
   return (
     <div>
       <p>HOME PAGE</p>
-      <Button>GO TO LOGIN</Button>
+      <Button onClick={handleOnClick}>GO TO LOGIN</Button>
     </div>
   )
 }
--- src/ui/login.tsx
+++ src/ui/login.tsx
@@ -2,15 +2,23 @@ import React, { useEffect } from "react"
 import IPage from "../interfaces/page"
 import logging from "../config/logger"
 import { Button } from "@mui/material"
+import { useHistory } from "react-router"
 
 const LoginPage: React.FC<IPage> = (props) => {
+  const history = useHistory()
+
+  const handleOnClick = () => {
+    history.push("/home")
+  }
+
   useEffect(() => {
     logging.info(`Loading ${props.name}`)
   }, [])
+
   return (
     <div>
       <p>LOGIN PAGE</p>
-      <Button>GO TO HOME</Button>
+      <Button onClick={handleOnClick}>GO TO HOME</Button>
     </div>
   )
 }
