bc32472b3e7f5e82a70984f2d47f171a03f4aaca
GACOSTA1988 <georgeacostar@gmail.com>
2021-11-01 19:52:11 -0700

    updates protected routes

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,19 +1,29 @@
 import IRoute from "../interfaces/route"
 import HomePage from "../ui/home"
 import LoginPage from "../ui/login"
+import RegisterPage from "../ui/register"
 
 const routes: IRoute[] = [
   {
-    path: "/",
+    path: "/Login",
     name: "Login Page",
     component: LoginPage,
     exact: true,
+    protected: false,
+  },
+  {
+    path: "/Register",
+    name: "Register Page",
+    component: RegisterPage,
+    exact: true,
+    protected: false,
   },
   {
     path: "/home",
     name: "Home Page",
     component: HomePage,
     exact: true,
+    protected: true,
   },
 ]
 
